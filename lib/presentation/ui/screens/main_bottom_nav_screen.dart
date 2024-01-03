ead2c0a5e704193e83dd4c1c1063c1d5eb32a6b8
csearafat01 <54732088+csearafat01@users.noreply.github.com>
2024-01-04 01:22:50 +0600

    added MainBottomNavScreen

--- lib/presentation/ui/screens/main_bottom_nav_screen.dart
+++ lib/presentation/ui/screens/main_bottom_nav_screen.dart
@@ -11,9 +11,42 @@ class _MainBottomNavScreenState extends State<MainBottomNavScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Main Bottom Nav Bar"),
+      backgroundColor: Colors.grey.shade200,
+      appBar: AppBar(
+
+        title: const Text('Innovation App'),
+        titleSpacing: 10,
+        ///centerTitle: true,
+        toolbarHeight: 60,
+        toolbarOpacity: 1,
+        elevation: 6,
+        backgroundColor: Colors.green,
+        actions: [
+          IconButton(onPressed: (){}, icon: Icon(Icons.comment)),
+          IconButton(onPressed: (){}, icon: Icon(Icons.search)),
+          IconButton(onPressed: (){}, icon: Icon(Icons.settings)),
+          IconButton(onPressed: (){}, icon: Icon(Icons.email))
+        ],
+
       ),
+      floatingActionButton: FloatingActionButton(
+        elevation: 18,
+        child: Icon(Icons.add),
+        backgroundColor: Colors.green,
+        onPressed: (){},
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 1,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.message), label: 'Message'),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+        onTap: (int index){
+
+        },
+      ),
+
     );
   }
 }
